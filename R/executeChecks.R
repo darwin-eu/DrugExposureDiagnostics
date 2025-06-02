b1ce92a40527850295787baea935bc7037628626
Ger Inberg <ginberg@gmail.com>
2025-06-02 12:04:40 +0200

    test

--- R/executeChecks.R
+++ R/executeChecks.R
@@ -474,7 +474,7 @@ executeChecksSingleIngredient <- function(cdm,
   )
 
   # add summary table
-  if ("diagnosticsSummary" %in% checks) {
+  if ("diagnosticsSummaryTest" %in% checks) {
     if (verbose == TRUE) {
       start <- printDurationAndMessage("Progress: create diagnosticsSummary", start)
     }
